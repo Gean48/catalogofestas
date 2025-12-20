3e64f06ac4ce7b9685a103e97162790d2ab59c6f
Gean48 <61244326+Gean48@users.noreply.github.com>
2025-12-20 10:19:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function contato(produto){
-    const numero = "5599999999999"; // coloque seu numero com 55 + DDD
+    const numero = "5533999140951"; // coloque seu numero com 55 + DDD
     const mensagem = encodeURIComponent("Olá! Tenho interesse no " + produto);
     window.open(`https://wa.me/${numero}?text=${mensagem}`, "_blank");
 }
