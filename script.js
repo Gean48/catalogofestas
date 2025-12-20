dd724c12004caeb22246f68ef79390eb5865acc2
Gean48 <61244326+Gean48@users.noreply.github.com>
2025-12-20 10:10:50 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function contato(produto){
-    const numero = "5599999999999"; // coloque seu numero com 55 + DDD
-    const mensagem = encodeURIComponent("Olá! Tenho interesse no " + produto);
-    window.open(`https://wa.me/${numero}?text=${mensagem}`, "_blank");
-}
