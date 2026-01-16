912e3654b1f9aabdee4fecf9064be8c676229fad
Gean48 <61244326+Gean48@users.noreply.github.com>
2026-01-16 14:43:44 -0300

    Update contato function to use link instead of image

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function contato(nome, valor, imagem){
+function contato(nome, valor, link){
   const numero = "5533999140951";
   const mensagem =
-    `Olá! Tenho interesse no ${nome}\nValor: ${valor}\n${imagem}`;
+`Olá! Tenho interesse no *${nome}*
+Valor: ${valor}
+Link do kit: ${link}`;
+  
   window.open(
     `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`,
     "_blank"
