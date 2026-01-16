ee786184eacf8eb8ae3a58acf76b0d6b08ce3fec
Gean48 <61244326+Gean48@users.noreply.github.com>
2026-01-16 14:34:11 -0300

    Refactor contato function and remove unused functions

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 function contato(nome, valor, imagem){
   const numero = "5533999140951";
-
-  const linkImagem = window.location.href.replace("index.html","") + imagem;
-
-  const mensagem = encodeURIComponent(
-    `Olá! Tenho interesse no *${nome}*\n` +
-    `💰 Valor do aluguel: ${valor}\n` +
-    `📸 Imagem do kit: ${linkImagem}`
+  const mensagem =
+    `Olá! Tenho interesse no ${nome}\nValor: ${valor}\n${imagem}`;
+  window.open(
+    `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`,
+    "_blank"
   );
-
-  window.open(`https://wa.me/${numero}?text=${mensagem}`, "_blank");
-}
-
-function verMais(nome, descricao, valor){
-  document.getElementById("modalTitulo").innerText = nome;
-  document.getElementById("modalDescricao").innerText = descricao;
-  document.getElementById("modalPreco").innerText = "Valor do aluguel: " + valor;
-
-  document.getElementById("modal").style.display = "flex";
-}
-
-function fecharModal(){
-  document.getElementById("modal").style.display = "none";
 }
