891b6f189422311ab36bf65ac048394350bb2d05
Gean48 <61244326+Gean48@users.noreply.github.com>
2026-01-16 14:24:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function contato(produto){
-    const numero = "5533999140951"; // coloque seu numero com 55 + DDD
-    const mensagem = encodeURIComponent("Olá! Tenho interesse no " + produto);
-    window.open(`https://wa.me/${numero}?text=${mensagem}`, "_blank");
+function contato(nome, valor, imagem){
+  const numero = "5533999140951";
+
+  const linkImagem = window.location.href.replace("index.html","") + imagem;
+
+  const mensagem = encodeURIComponent(
+    `Olá! Tenho interesse no *${nome}*\n` +
+    `💰 Valor do aluguel: ${valor}\n` +
+    `📸 Imagem do kit: ${linkImagem}`
+  );
+
+  window.open(`https://wa.me/${numero}?text=${mensagem}`, "_blank");
+}
+
+function verMais(nome, descricao, valor){
+  document.getElementById("modalTitulo").innerText = nome;
+  document.getElementById("modalDescricao").innerText = descricao;
+  document.getElementById("modalPreco").innerText = "Valor do aluguel: " + valor;
+
+  document.getElementById("modal").style.display = "flex";
+}
+
+function fecharModal(){
+  document.getElementById("modal").style.display = "none";
 }
